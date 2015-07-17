3b8dca5684346af448ec3ae3ada5b4244153ef6b
Jason Pepas <jasonpepas@gmail.com>
2015-07-16 23:53:52 -0500

    Episode 4: Trivial data store protocol.  ViewController shows count of tasks.

--- TODOApp/ViewController.swift
+++ TODOApp/ViewController.swift
@@ -8,11 +8,86 @@
 
 import UIKit
 
+protocol TaskStoreProtocol
+{
+    func addTask(taskTitle: String)
+    func tasksCount() -> Int
+}
+
+class InMemoryTaskStore: TaskStoreProtocol
+{
+    private var tasks: [String] = [String]()
+    
+    func addTask(taskTitle: String)
+    {
+        tasks.append(taskTitle)
+    }
+    
+    func tasksCount() -> Int
+    {
+        return count(tasks)
+    }
+}
+
+class NSUserDefaultsTaskStore: TaskStoreProtocol
+{
+    init()
+    {
+        tasks = _loadTasksFromDisk()
+    }
+    
+    func addTask(taskTitle: String)
+    {
+        tasks.append(taskTitle)
+        _writeTasksToDisk()
+    }
+    
+    func tasksCount() -> Int
+    {
+        return count(tasks)
+    }
+    
+    private var tasks: [String] = [String]()
+
+    private let _tasksNSUserDefaultsKey: String = "_tasksNSUserDefaultsKey"
+    
+    private func _loadTasksFromDisk() -> [String]
+    {
+        if var tasks = NSUserDefaults.standardUserDefaults().arrayForKey(_tasksNSUserDefaultsKey) as? [String]
+        {
+            return tasks
+        }
+        else
+        {
+            return [String]()
+        }
+    }
+    
+    private func _writeTasksToDisk()
+    {
+        NSUserDefaults.standardUserDefaults().setObject(tasks, forKey: _tasksNSUserDefaultsKey)
+    }
+}
+
 class ViewController: UIViewController, ModalTaskCapturingDelegateProtocol
 {
+    @IBOutlet weak var taskCountLabel: UILabel!
+    private var taskStore: TaskStoreProtocol = NSUserDefaultsTaskStore()
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        _updateTaskCountLabel()
+    }
+    
+    private func _updateTaskCountLabel()
+    {
+        taskCountLabel.text = "\(taskStore.tasksCount())"
     }
 }
 
@@ -21,8 +96,6 @@ extension ViewController
 {
     @IBAction func buttonDidGetTapped(sender: AnyObject)
     {
-        debugPrintln("button!")
-        
         let vc = NewTaskViewController.instantiateFromStoryboard()
         vc.taskCapturingDelegate = self
         
@@ -35,13 +108,13 @@ extension ViewController: ModalTaskCapturingDelegateProtocol
 {
     func taskCapturingModalDidFinish(taskTitle: String)
     {
-        debugPrintln("done!!! with task title: \(taskTitle)")
+        taskStore.addTask(taskTitle)
+        _updateTaskCountLabel()
         presentedViewController?.dismissViewControllerAnimated(true, completion: nil)
     }
     
     func taskCapturingModalDidCancel()
     {
-        debugPrintln("cancel!!!")
         presentedViewController?.dismissViewControllerAnimated(true, completion: nil)
     }
 }
